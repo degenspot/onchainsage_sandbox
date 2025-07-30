f7add24c9a535f6499e53441370cdd2a0f3b5478
nafiuishaaq <nafiuig@gmail.com>
2025-07-30 12:37:03 +0100

    updated the html

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { AchievementsModule } from './achievements/achievements.module';
 import { ChallengeModule } from './challenge/challenge.module';
 import { PredictionMarketsModule } from './prediction-markets/prediction-markets.module';
+import { TournamentsModule } from './tournaments/tournaments.module';
 
 @Module({
-  imports: [AchievementsModule, ChallengeModule, PredictionMarketsModule],
+  imports: [AchievementsModule, ChallengeModule, PredictionMarketsModule, TournamentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
